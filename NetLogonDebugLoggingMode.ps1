04f2c40e9f5bd80e5c40fd4caf30e6989d9956dc
Jeff Patton <jspatton@ku.edu>
2015-06-19 16:46:19 -0500

    Changed the code underneath to do the conversion to MB, just have to enter in 20 the code will convert that to the equivalent MB

---
+++ NetLogonDebugLoggingMode.ps1
@@ -0,0 +1,28 @@
+﻿Configuration NetLogonDebugLoggingMode
+{
+    param
+    (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ComputerName
+    )
+
+    Import-DscResource -ModuleName PTECH_DSDiagnosticLogging;
+    
+    Node $ComputerName
+    {
+        #
+        #region Enable NETLOGON debug logging
+        #
+        PTECH_NetLogonDebugLoggingMode EnableDebugLog
+        {
+            DebugMode = 'Enable'
+            Logsize = 20
+            Restart = $true
+            Ensure = 'Present'
+            }
+        #
+        #endregion
+        #
+        }
+    }
